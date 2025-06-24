88e018137c0f44970d1288bea3721f6ecb8ab1df
Flaxx99 <luis.vidal.b90@gmail.com>
2025-06-23 20:55:50 -0600

    sincronizar documentos y templates desde yoshy-apps

--- usaer_system/documentos/migrations/0001_initial.py
+++ usaer_system/documentos/migrations/0001_initial.py
@@ -11,7 +11,7 @@ class Migration(migrations.Migration):
     initial = True
 
     dependencies = [
-        ('alumnos', '0002_alter_alumno_clasificacion_alter_alumno_grado_and_more'),
+        ('alumnos', '0002_alter_alumno_profesor'),
         migrations.swappable_dependency(settings.AUTH_USER_MODEL),
     ]
 
