6473a869cc9c5d1071a6abbfc1112bff414ebf7b
Luis Vidal <luis.vidal.b90@gmail.com>
2026-06-02 10:15:06 -0600

    test(rac): finalize migration to APITestCase and fix visibility tests (slice 3)

--- usaer_system/rac/tests.py
+++ usaer_system/rac/tests.py
@@ -273,73 +273,16 @@ class RACExportTest(APITestCase):
         self.client.force_authenticate(self.admin_user)
         response = self.client.get(reverse('rac:registros-list'))
         self.assertEqual(response.status_code, 200)
-        self.assertContains(response, "Exportar Todo a Excel")
+        # API devuelve JSON; comprobamos que la respuesta incluye el conteo y estructura esperada
+        data = response.json()
+        self.assertIn('count', data)
 
-        self.client.force_authenticate(self.teacher1)
-        response = self.client.get(reverse('rac:registros-list'))
-        self.assertEqual(response.status_code, 200)
-        self.assertNotContains(response, "Exportar Todo a Excel")
-
-
-class RACExportTest(APITestCase):
-
-    def setUp(self):
-        '''Set up data for each test.'''
-        self.client = APIClient()
-        # Crear usuarios
-        self.admin_user = User.objects.create_user(email='admin@test.com', numero_empleado='123', password='password', role='ADMIN', first_name='Admin', last_name='User')
-        self.teacher1 = User.objects.create_user(email='teacher1@test.com', numero_empleado='456', password='password', role='MAESTRO_APOYO', first_name='Maestro', last_name='Uno')
-        self.teacher2 = User.objects.create_user(email='teacher2@test.com', numero_empleado='789', password='password', role='MAESTRO_APOYO', first_name='Maestra', last_name='Dos')
-
-        # Crear escuelas
-        self.school1 = Escuela.objects.create(nombre="Escuela Primaria Benito Juarez", cct="12345", clave_estatal="123", nivel="PRIMARIA")
-        self.school2 = Escuela.objects.create(nombre="Escuela Secundaria Tecnica 34", cct="67890", clave_estatal="456", nivel="SECUNDARIA")
-
-        # Crear alumnos
-        self.student1_t1 = Alumno.objects.create(nombres="Juan", apellido_paterno="Perez", sexo="H", profesor=self.teacher1, escuela=self.school1, edad=8, curp="CURP1")
-        self.student2_t1 = Alumno.objects.create(nombres="Maria", apellido_paterno="Gomez", sexo="M", profesor=self.teacher1, escuela=self.school1, edad=9, curp="CURP2")
-        self.student1_t2 = Alumno.objects.create(nombres="Pedro", apellido_paterno="Lopez", sexo="H", profesor=self.teacher2, escuela=self.school2, edad=10, curp="CURP3")
-
-        # Crear registros RAC
-        self.rac1 = RegistroRAC.objects.create(alumno=self.student1_t1, maestro_apoyo=self.teacher1, escuela_regular=self.school1, escuela_basica=self.school1, clasificacion='DISCAPACIDAD', subclasificacion='DI', service_type='USAER')
-        self.rac2 = RegistroRAC.objects.create(alumno=self.student2_t1, maestro_apoyo=self.teacher1, escuela_regular=self.school1, escuela_basica=self.school1, clasificacion='APTITUDES_SOBRESALIENTES', subclasificacion='ASI', service_type='USAER')
-        self.rac3 = RegistroRAC.objects.create(alumno=self.student1_t2, maestro_apoyo=self.teacher2, escuela_regular=self.school2, escuela_basica=self.school2, clasificacion='TRASTORNOS', subclasificacion='TDAH', service_type='USAER')
-
-    def get_test_template(self):
-        wb = openpyxl.Workbook()
-        wb.create_sheet("RAC")
-        wb.create_sheet("ESTADÍSTICA POBLACIÓN 2025")
-        template_in_memory = BytesIO()
-        wb.save(template_in_memory)
-        template_in_memory.seek(0)
-        return template_in_memory
-
-    def test_export_rac_excel_view_for_teacher(self):
-        '''Prueba que un maestro exporte sus registros y el contenido sea correcto.'''
-        self.client.force_authenticate(self.teacher1)
-        response = self.client.get(reverse('rac:exportar_excel'), HTTP_IS_TEST='True')
-        self.assertEqual(response.status_code, 200)
-
-    def test_export_all_rac_excel_view_for_admin(self):
-        '''Prueba que el admin exporte todos los registros y el contenido sea correcto.'''
+        # Comprobamos que el endpoint de exportar-todo está accesible por admin y denegado para maestro
         self.client.force_authenticate(self.admin_user)
-        response = self.client.get(reverse('rac:exportar_todo'), HTTP_IS_TEST='True')
-        self.assertEqual(response.status_code, 200)
-
-    def test_export_all_permission_denied_for_teacher(self):
-        '''Prueba que un maestro no pueda acceder a la exportación total.'''
-        self.client.force_authenticate(self.teacher1)
-        response = self.client.get(reverse('rac:exportar_todo'))
-        self.assertEqual(response.status_code, 403)
-
-    def test_export_all_button_visibility(self):
-        '''Prueba la visibilidad del botón de exportar todo según el rol.'''
-        self.client.force_authenticate(self.admin_user)
-        response = self.client.get(reverse('rac:registros-list'))
-        self.assertEqual(response.status_code, 200)
-        self.assertContains(response, "Exportar Todo a Excel")
+        resp_admin = self.client.get(reverse('rac:exportar_todo'), HTTP_IS_TEST='True')
+        self.assertEqual(resp_admin.status_code, 200)
 
         self.client.force_authenticate(self.teacher1)
-        response = self.client.get(reverse('rac:registros-list'))
-        self.assertEqual(response.status_code, 200)
-        self.assertNotContains(response, "Exportar Todo a Excel")
+        resp_teacher = self.client.get(reverse('rac:exportar_todo'))
+        self.assertEqual(resp_teacher.status_code, 403)
+
